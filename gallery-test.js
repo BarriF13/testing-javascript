fd83b97a2cd1acea4d7de7dc3e2dbc9c309d222e
Barri <info@bdesignstudio.co.uk>
2020-05-19 16:29:25 +0100

    Third test passing

--- gallery-test.js
+++ gallery-test.js
@@ -1,3 +1,7 @@
 var gal = Gallery.create("gal-1");
 
 TEST.areEqual(typeof gal, "object" , "Gallery should be an object");
+
+TEST.areEqual(gal.el.id, "gal-1" , "Gallery.el should be the one we specified");
+
+TEST.areEqual(gal.idx, 0, "Gallery index should be start at zero");
